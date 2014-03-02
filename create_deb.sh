c64d95461722b5b29b1e5246d9e8b0195f9065d4
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2014-03-03 00:36:23 +0700

    automatic detect the architecture and codename

--- create_deb.sh
+++ create_deb.sh
@@ -38,12 +38,14 @@ chmod 0644 debian_package/usr/share/pixmaps/vkphotosync.png
 VER=`git describe --match=v* HEAD --tags`
 VER=`echo $VER| sed 's/^v//'`
 SIZE=`du -cb ./debian_package/usr | grep total | awk '{print int($1/1024)}'`
+ARCH=`dpkg --print-architecture`
+CODENAME=`lsb_release -c | awk '{print $2}'`
 
 echo "Package: vkphotosync
 Version: $VER
 Section: net
 Priority: optional
-Architecture: amd64
+Architecture: $ARCH
 Installed-Size: $SIZE
 Depends: libc6 (>= 2.14), libgcc1 (>= 1:4.1.1), libqt4-webkit (>= 4:4.8.0), libqt4-network (>= 4:4.8.0), libqt4-xml (>= 4:4.8.0), libqtcore4 (>= 4:4.8.0), libqtgui4 (>= 4:4.8.0)
 Homepage: https://github.com/AlekseyDurachenko/vkPhotoSync
@@ -58,5 +60,5 @@ cd ..
 chmod 644 debian_package/DEBIAN/*
 
 fakeroot dpkg-deb --build debian_package
-mv debian_package.deb "vkphotosync_""$VER""_amd64.deb"
-lintian "vkphotosync_""$VER""_amd64.deb"
+mv debian_package.deb "vkphotosync_""$VER~$CODENAME""_""$ARCH"".deb"
+lintian "vkphotosync_""$VER~$CODENAME""_""$ARCH"".deb"
